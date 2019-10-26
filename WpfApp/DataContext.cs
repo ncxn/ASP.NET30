a8aef0d427cf81cd54106ea9134996ef94e8f806
ncxn <ncxn@outlook.com>
2019-10-26 16:14:10 +0700

    wpf

---
+++ WpfApp/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp
+{
+    public class DataContext:DbContext
+    {
+        // DbSet properties declarations...
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
