389ab122e56da76131c806b8d20cc533fef6009a
ncxn <ncxn@outlook.com>
2019-10-29 17:20:58 +0700

    login

---
+++ WebMVC/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Phải nhập tên đăng nhập")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+}
