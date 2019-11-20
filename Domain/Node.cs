8f62e952dfdcc83fd6326b67da9ac40519253e2b
ncxn <ncxn@outlook.com>
2019-11-20 16:35:21 +0700

    add domain layer

---
+++ Domain/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Node
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Url { get; set; }
+
+    }
+}
---
+++ Domain/Node_Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    class Node_Type
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Decs { get; set; }
+        public string Url { get; set; }
+    }
+}
