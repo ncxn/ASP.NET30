9112f111b9e5338f1865f726a88b4712aa95c40d
ncxn <ncxn@outlook.com>
2019-11-23 11:28:59 +0700

    del

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddTransient<AppDb>(_ => new AppDb(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
